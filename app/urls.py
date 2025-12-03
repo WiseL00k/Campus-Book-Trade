18758b379e6300a68bdd1c6f6d46eee44c40b6d5
WiseLook <1656438881@qq.com>
2025-12-03 11:23:08 +0800

    style: change file.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
-# app/urls.py
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'app'
@@ -7,5 +7,7 @@ urlpatterns = [
     path('index', views.IndexView.as_view(), name='index'),
     path('detail/<int:pk>', views.DetailView.as_view(), name='detail'),
     path('commit', views.CommitView.as_view(), name='commit'),
-    path('search/', views.SearchView.as_view(), name='search'),  # 添加搜索路由
+    path('login/', LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register')
 ]
